197465adf03424ee1d6adc64da4a5ceaad4ea8e0
Casalcuin <casalcuin.crow@gmail.com>
2022-05-02 19:45:37 -0400

    Refactored Javascript for Pixel maker

--- designs.js
+++ designs.js
@@ -1,10 +1,33 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
 
 // Your code goes here!
 
+function makeGrid() {
+    const getGridHeight = document.querySelector('#inputHeight').value;
+    const getGridWidth = document.querySelector('#inputWidth').value;
+    const populateGrid = document.querySelector('#pixelCanvas');
+    populateGrid.innerHTML = "";
+    for (let hight = 0; hight < getGridHeight; hight++) {
+        const row = populateGrid.insertRow(-1);
+        for (let width = 0; width < getGridWidth; width++) {
+            const cell = row.insertCell(-1);
+            cell.addEventListener('mousedown', function () {
+                const color = document.getElementById('colorPicker').value; // Select color input
+                this.style.backgroundColor = color;
+            })
+
+
+        }
+    }
 }
+//Binds the submit button to the makeGrid function, and prevents the page from reloading
+const sizePicker = document.querySelector('#sizePicker');// Select size input
+sizePicker.addEventListener('submit', function (event) {
+    event.preventDefault();
+    makeGrid();// When size is submitted by the user, call makeGrid()
+
+});
+
+
+
+
+
